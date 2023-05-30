06277645f0c100219a17fe83becd735209791576
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2023-05-30 13:04:49 -0700

    Update conf.py

--- book/sphinx/conf.py
+++ book/sphinx/conf.py
@@ -2,7 +2,7 @@
 from __future__ import annotations
 
 master_doc = "index"
-project = "cvxsimulator"
+project = "cvxrisk"
 
 source_suffix = {
     ".rst": "restructuredtext",
@@ -37,7 +37,7 @@ napoleon_numpy_docstring = True
 # HTML -----------------------------------------------------------------
 html_favicon = "_static/logo.jpeg"
 html_logo = "_static/logo.jpeg"
-html_title = "cvxsimulator Documentation"
+html_title = "cvxrisk Documentation"
 html_show_sourcelink = True
 html_css_files = [
     "custom.css",
