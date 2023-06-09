8fef8d25867b11d30b1d7d749c4149d11bb6a5bf
Thomas Schmelzer <thomas.schmelzer@adia.ae>
2023-06-08 23:05:37 -0700

    Using parameter correctly

---
+++ tests/test_risk/test_cvar/test_cvar.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Import necessary libraries
+from __future__ import annotations
+
+import cvxpy as cvx
+import numpy as np
+import pytest
+
+from cvx.risk.cvar.cvar import CVar
+
+
+def test_estimate_risk():
+    """Test the estimate_risk() method"""
+    alpha = 0.95
+    model = CVar(alpha)
+    np.random.seed(42)
+    R = np.random.randn(50, 10)
+    weights = cvx.Variable(10)
+    risk = model.estimate_risk(weights, R=R)
+    prob = cvx.Problem(cvx.Minimize(risk), [cvx.sum(weights) == 1, weights >= 0])
+    prob.solve()
+    assert prob.value == pytest.approx(0.5058720677762698)
