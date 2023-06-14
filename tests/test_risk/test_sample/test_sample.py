02a712f39c7df13651a6382c4ad571ebbef01bab
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2023-06-14 10:31:39 -0700

    check error is raised

--- tests/test_risk/test_sample/test_sample.py
+++ tests/test_risk/test_sample/test_sample.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 
 import numpy as np
 import pandas as pd
+import pytest
 
 from cvx.risk.factor import FundamentalFactorRiskModel
 from cvx.risk.sample import SampleCovariance
@@ -15,9 +16,12 @@ def test_sample():
     var = riskmodel.estimate_risk(np.array([1.0, 1.0])).value
     np.testing.assert_almost_equal(var, 4.0)
 
+
 def test_sample_not_psd():
     riskmodel = SampleCovariance(num=2)
-    riskmodel.cov.value = np.array([[-1.0, 0.5], [0.5, -2.0]])
+    with pytest.raises(ValueError):
+        riskmodel.cov.value = np.array([[-1.0, 0.5], [0.5, -2.0]])
+
 
 def test_sample_product():
     riskmodel = SampleCovariance_Product(num=2)
