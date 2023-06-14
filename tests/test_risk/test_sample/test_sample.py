1b0edb067b4713c973cc9b244f18f4cf36379f5a
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2023-06-14 10:32:56 -0700

    test raising error for non psd cov matrix (#22)
    * Update test_sample.py
    
    * check error is raised

--- tests/test_risk/test_sample/test_sample.py
+++ tests/test_risk/test_sample/test_sample.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 
 import numpy as np
 import pandas as pd
+import pytest
 
 from cvx.risk.factor import FundamentalFactorRiskModel
 from cvx.risk.sample import SampleCovariance
@@ -14,6 +15,12 @@ def test_sample():
     riskmodel.cov.value = np.array([[1.0, 0.5], [0.5, 2.0]])
     var = riskmodel.estimate_risk(np.array([1.0, 1.0])).value
     np.testing.assert_almost_equal(var, 4.0)
+
+
+def test_sample_not_psd():
+    riskmodel = SampleCovariance(num=2)
+    with pytest.raises(ValueError):
+        riskmodel.cov.value = np.array([[-1.0, 0.5], [0.5, -2.0]])
 
 
 def test_sample_product():
