eb6d5d60dac8912adcfb3debbe78abaf11e99e40
Thomas Schmelzer <thomas.schmelzer@adia.ae>
2023-06-18 00:01:09 -0700

    sample test

--- tests/test_risk/test_sample/test_sample.py
+++ tests/test_risk/test_sample/test_sample.py
@@ -34,13 +34,14 @@ def test_sample_product():
 def test_optimize_no_update():
     weights = cp.Variable(2)
     riskmodel = SampleCovariance_Product(num=2)
-    riskmodel.chol.value = SampleCovariance_Product.cholesky(
-        np.array([[1.0, 0.5], [0.5, 2.0]])
-    )
-    # riskmodel.cov.value = np.array([[1.0, 0.5], [0.5, 2.0]])
+
     problem = cp.Problem(
         cp.Minimize(riskmodel.estimate_risk(weights)),
         [cp.sum(weights) == 1.0, weights >= 0],
+    )
+
+    riskmodel.chol.value = SampleCovariance_Product.cholesky(
+        np.array([[1.0, 0.5], [0.5, 2.0]])
     )
     problem.solve()
     print(weights.value)
@@ -56,11 +57,13 @@ def test_optimize_no_update():
 def test_optimize_update():
     weights = cp.Variable(2)
     riskmodel = SampleCovariance(num=2)
-    riskmodel.cov.value = np.array([[1.0, 0.5], [0.5, 2.0]])
+
     problem = cp.Problem(
         cp.Minimize(riskmodel.estimate_risk(weights)),
         [cp.sum(weights) == 1.0, weights >= 0],
     )
+
+    riskmodel.cov.value = np.array([[1.0, 0.5], [0.5, 2.0]])
     problem.solve()
     print(weights.value)
 
