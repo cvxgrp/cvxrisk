04a40ccf6c6db48316696409e429a26f9508e55a
Thomas Schmelzer <thomas.schmelzer@adia.ae>
2024-10-27 17:26:34 +1100

    moving linalg into risk to reduce level of relative imports

--- cvx/risk/factor/factor.py
+++ cvx/risk/factor/factor.py
@@ -20,7 +20,7 @@ from dataclasses import dataclass
 import cvxpy as cvx
 import numpy as np
 
-from ...linalg import cholesky
+from ..linalg import cholesky
 from ..bounds import Bounds
 from ..model import Model
 
