d0cb83cbd244b5c78c4a2517be54c52fae2bfeb4
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2025-06-10 14:24:53 +0400

    build cvxrisk folder

--- book/marimo/demo.py
+++ book/marimo/demo.py
@@ -5,45 +5,29 @@ import marimo
 __generated_with = "0.13.15"
 app = marimo.App(width="medium")
 
-
-@app.cell
-async def _():
-    # Check if running in WebAssembly environment
-    try:
-        import sys
-
-        if "pyodide" in sys.modules:
-            import micropip
-
-            await micropip.install("cvxrisk")
-            await micropip.install("cvxsimulator")
-
-    except ImportError:
-        pass
-
+with app.setup:
     import cvxpy as cp
     import marimo as mo
     import numpy as np
     import pandas as pd
     import polars as pl
+    from cvxsimulator import Builder
 
     from cvxrisk.portfolio import minrisk_problem
     from cvxrisk.sample import SampleCovariance
-    from cvxrisk.simulator import Builder
 
     pd.options.plotting.backend = "plotly"
 
+    from cvxsimulator import __version__ as simulator_version
+
     from cvxrisk import __version__ as risk_version
-    from cvxrisk.simulator import __version__ as simulator_version
 
     print(f"Risk version: {risk_version}")
     print(f"Simulator version: {simulator_version}")
 
-    return Builder, SampleCovariance, minrisk_problem, mo, cp, np, pl
-
 
 @app.cell
-def _(mo, pl):
+def _():
     # Load some historic stock prices
     prices = pl.read_csv(str(mo.notebook_location() / "public" / "stock_prices.csv"), try_parse_dates=True)
 
@@ -55,7 +39,7 @@ def _(mo, pl):
 
 
 @app.cell
-def _(Builder, SampleCovariance, cp, minrisk_problem, np, prices, returns):
+def _(prices, returns):
     cov = returns.ewm(com=60, min_periods=100).cov().dropna(axis=0, how="all")
     start = cov.index[0][0]
 
@@ -88,7 +72,7 @@ def _(Builder, SampleCovariance, cp, minrisk_problem, np, prices, returns):
 
 
 @app.cell
-def _(Builder, cp, minrisk_problem, np, prices, returns, start):
+def _(prices, returns, start):
     from cvxrisk.cvar import CVar
 
     _risk_model = CVar(alpha=0.80, n=40, m=20)
